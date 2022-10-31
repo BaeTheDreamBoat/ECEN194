e7756412147fefdf288918b6d1ba8faca61dce0c
Ryan <rb91827@gmail.com>
2022-10-31 13:00:22 -0500

    wrote all base functions and a test in tester file. Fails to make on dell

--- hacks/hack09/string_utils.c
+++ hacks/hack09/string_utils.c
@@ -15,26 +15,82 @@ void replaceChar(char *s, char oldChar, char newChar){
     char *copy[length];
     copy[length]='\0';
     for (int i=0;i<length;++i){
-        copy[i]=s[i];
+        *copy[i]=s[i];
         if (s[i]==oldChar){
-            copy[i]==newChar;
+            *copy[i]=newChar;
         }
     }
     return;
 }
 
 char * replaceCharCopy(const char *s, char oldChar, char newChar){
-
+    int length = strlen(s);
+    char *copy[length];
+    copy[length]='\0';
+    for (int i=0;i<length;++i){
+        *copy[i]=s[i];
+        if (s[i]==oldChar){
+            *copy[i]=newChar;
+        }
+    }
+    return *copy;   
 }
 
 void removeChar(char *s, char c){
-
+    int oldLength=strlen(s);
+    int newLength=oldLength;
+    for (int i=0;i<oldLength;++i){
+        if (s[i]==c);
+        --newLength;
+    }
+    char *copy[newLength];
+    for (int i=0;i<newLength;++i){
+        if (s[i]!=c){
+        *copy[i]=s[i];
+        }
+    }
+    for (int i=0;i<oldLength;++i) {
+        if(i<newLength) {
+            s[i]= *copy[i];
+        }
+        else {
+            s[i]='\0';
+        }
+    }
+    return;
 }
 
 char * removeCharCopy(const char *s, char c){
-
+    int oldLength=strlen(s);
+    int newLength=oldLength;
+    for (int i=0;i<oldLength;++i){
+        if (s[i]==c);
+        --newLength;
+    }
+    char *copy[newLength];
+    for (int i=0;i<oldLength;++i){
+        if(i<newLength){
+            if (s[i]!=c){
+            *copy[i]=s[i];
+            }
+        }    
+    }
+    return *copy;
 }
 
 char **lengthSplit(const char *s, int n){
-    
+    int LenghtOfArr=0;
+    if ((strlen(s)%n)>0) {
+        LenghtOfArr=(strlen(s)/n)+1;
+    }
+    else {
+        LenghtOfArr=(strlen(s)/n);
+    }
+    char **arr[LenghtOfArr][n];
+    for (int i=0;i<LenghtOfArr;++i){
+        for(int j=0;j<n;++j){
+            **arr[i][j]= s[(i*3)+j];
+        }
+    }
+    return **arr;
 }
