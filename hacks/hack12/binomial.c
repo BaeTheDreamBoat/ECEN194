3a7fbef311449e5bd7dc46f15b0a3e254c6051dc
Ryan <rb91827@gmail.com>
2022-11-28 23:12:42 -0600

    hack 12 not done yet

---
+++ hacks/hack12/binomial.c
@@ -0,0 +1,15 @@
+/**
+ * Ryan Brown 
+ * 11/28/22
+ * 
+ * definitions of functions for the binomial program
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "binomial.h"
+
+long choose(int n, int j){
+    
+}
---
+++ hacks/hack12/binomial.h
@@ -0,0 +1,9 @@
+/**
+ * Ryan Brown
+ * 11/28/22
+ * 
+ * dictionary of utility functions for the
+ * binomial program
+ */
+
+long choose(int n, int j);
